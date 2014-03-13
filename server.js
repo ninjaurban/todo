25b9648a117fbb9c884bfc844ff2fbe83ad0ca80
Niklas Ekman <niklas@niklasekman.com>
2014-03-13 05:53:48 +0200

    Den första över r huvudtaget fungerande versionen

---
+++ server.js
@@ -0,0 +1,29 @@
+var http = require('http');
+var url = require('url');
+
+var items = [];
+
+var server = http.createServer(function(request, response) {
+	switch (request.method) {
+		case 'POST':
+			var item = '';
+			request.setEncoding('utf8');
+
+			request.on('data', function(chunk) {
+				item += chunk;
+			});
+
+			request.on('end', function() {
+				items.push(item);
+				response.end('OK\n');
+			});
+
+			break;
+
+		case 'GET':
+			response.end(items.join(', ') + '\n');
+			break;
+	}
+});
+
+server.listen(3000);
